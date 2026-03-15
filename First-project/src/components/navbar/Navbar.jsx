2a7f8deb80e070186841607b064f6c5bd5e707e9
Shashank Lakhera <lakherashashank70@gmail.com>
2026-03-15 13:19:48 +0530

    improved Navbar padding

--- First-project/src/components/navbar/Navbar.jsx
+++ First-project/src/components/navbar/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
     return (
         <div className={`w-full flex shadow-[0_5px_15px_rgb(0,0,0,0.2)] bg-black/15 backdrop-blur-sm justify-between px-8 
         fixed ${isVisible ? 'translate-y-0' : '-translate-y-full'} `}>
-            <h1 className="py-5 font-bold tracking-wider uppercase text-4xl ">Our Products</h1>
+            <h1 className="py-3 font-bold tracking-wider uppercase text-4xl ">Our Products</h1>
             <div className=' flex text-xl gap-4 items-center font-semibold'>
                 <p className='cursor-pointer hover:underline '>Cart</p>
                 <p className='cursor-pointer hover:underline '>Login/Signup</p>
